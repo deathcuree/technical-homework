b98d0d0388426f38d21ea35032a1cb62a8ffec32
deathcuree <nikaandaya21@gmail.com>
2024-02-21 09:52:47 +0800

    login component directing to home

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,12 @@ export class LoginComponent {
     password: ['', Validators.required],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private msgService: MessageService
+  ) {}
 
   get email() {
     return this.loginForm.controls['email'];
@@ -22,5 +28,38 @@ export class LoginComponent {
 
   get password() {
     return this.loginForm.controls['password'];
+  }
+
+  loginUser() {
+    const { email, password } = this.loginForm.value;
+    this.authService.getUserByEmail(email as string).subscribe(
+      (response) => {
+        if (response.length > 0 && response[0].password === password) {
+          sessionStorage.setItem('email', email as string);
+          this.router.navigate(['/home']);
+        } else if (response.length === 0) {
+          this.msgService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail:
+              'User not found. Please check your email or register for an account.',
+          });
+        } else {
+          this.msgService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail:
+              'Incorrect password. Please check your password and try again.',
+          });
+        }
+      },
+      (error) => {
+        this.msgService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Something went wrong',
+        });
+      }
+    );
   }
 }
